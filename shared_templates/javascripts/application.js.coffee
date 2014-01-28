647e2b24f4cb33152e09fca72c7764e2dad6ebd3
Robert Evans <robert@codewranglers.org>
2014-01-27 18:37:57 -0800

    making this sinatra template as ready to go from the start as possible

--- shared_templates/javascripts/application.js.coffee
+++ shared_templates/javascripts/application.js.coffee
@@ -1,1 +1,5 @@
-//= require jquery
+//= require jquery/jquery.min
+//= require modernizr/modernizr
+//= require bootstrap/dist/js/bootstrap.min
+//= require sugar/release/sugar-full.min
+//= require angular/angular.min
--- sinatra/sinatra.rb
+++ sinatra/sinatra.rb
@@ -24,6 +24,10 @@ root "sinatra" do
     template_path.join('config/application.rb.erb'),
     { app_classname: 'GetTheRundown' }
 
+  template "config/configuration.rb",
+    template_path.join('config/configuration.rb.erb'),
+    { app_classname: 'GetTheRundown' }
+
   template "app/api.rb",
     template_path.join('app/api.rb.erb'),
     { app_classname: 'GetTheRundown' }
